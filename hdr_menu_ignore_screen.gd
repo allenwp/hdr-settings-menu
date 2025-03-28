304a40d3ea955dc40e14adf446589b0c31000a47
Allen Pestaluky <allenpestaluky@gmail.com>
2025-03-28 14:36:11 -0400

    Fixed ignore screen luminance style to not require interacting with other sytles

--- hdr_menu_ignore_screen.gd
+++ hdr_menu_ignore_screen.gd
@@ -7,8 +7,8 @@ const default_max_lum = 1000
 func refresh_menu() -> void:
 	super()
 	var window = get_window()
-	%BrightnessSlider.value = get_window().hdr_output_reference_luminance
-	%BrightnessLabel.text = str(get_window().hdr_output_reference_luminance)
+	%BrightnessSlider.value = window.hdr_output_reference_luminance
+	%BrightnessLabel.text = str(window.hdr_output_reference_luminance)
 	%MaxLumSlider.value = window.hdr_output_max_luminance
 	%MaxLumLabel.text = str(window.hdr_output_max_luminance)
 
@@ -22,10 +22,14 @@ func _on_max_lum_slider_value_changed(value: float) -> void:
 
 
 func _on_reset_brightness_pressed() -> void:
-	get_window().hdr_output_use_screen_luminance = false
+	_on_interact()
 	get_window().hdr_output_reference_luminance = default_reference_lum
 
 
 func _on_reset_max_lum_pressed() -> void:
-	get_window().hdr_output_use_screen_luminance = false
+	_on_interact()
 	get_window().hdr_output_max_luminance = default_max_lum
+
+
+func _on_interact() -> void:
+	get_window().hdr_output_use_screen_luminance = false
